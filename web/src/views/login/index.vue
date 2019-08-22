5bd0739bb1e05230ac8f1aca28e9758a7dc58ce8
root <root@vultr.guest>
2019-08-22 07:26:39 +0000

    change login welcome

--- web/src/views/login/index.vue
+++ web/src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">你好，小小格哥</h3>
       </div>
 
       <el-form-item prop="username">
